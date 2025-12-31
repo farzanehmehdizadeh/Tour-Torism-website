e45da6325d92213448e65d6cda841106568b1ab6
farzanehmehdizadeh <146867882+farzanehmehdizadeh@users.noreply.github.com>
2025-12-31 20:13:24 +0330

    Delete news directory

--- news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import News
-
-class NewsForm(forms.ModelForm):
-    class Meta:
-        model = News
-        fields = ['title_news', 'description_news', 'image_news']
