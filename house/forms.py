61968b7930bf09aec13032b936a5768c01265915
farzanehmehdizadeh <146867882+farzanehmehdizadeh@users.noreply.github.com>
2025-12-31 20:13:12 +0330

    Delete house directory

--- house/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django import forms
-from .models import house
-
-
-class houseForm(forms.ModelForm):
-    class Meta:
-        model = house
-        fields = ['name_house', 'description_house', 'image_house']
